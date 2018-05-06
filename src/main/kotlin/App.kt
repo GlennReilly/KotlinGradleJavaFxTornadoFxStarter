feb0985327d3710c443857ad8b66ea1bbfeb6586
Glenn Reilly <glenn.reilly@gmail.com>
2018-05-06 13:31:44 +1000

    init commit

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,23 @@
+import javafx.application.Application
+import javafx.application.Application.launch
+import javafx.scene.Scene
+import javafx.scene.control.Label
+import javafx.scene.layout.VBox
+import javafx.stage.Stage
+
+fun main(args: Array<String>) {
+    launch(HelloWorld::class.java)
+}
+
+class HelloWorld : Application(){
+    override fun start(primaryStage: Stage?) {
+        val layout = VBox().apply{
+            children.add(Label("Hello World"))
+        }
+
+        primaryStage?.run {
+            scene = Scene(layout)
+            show()
+        }
+    }
+}
