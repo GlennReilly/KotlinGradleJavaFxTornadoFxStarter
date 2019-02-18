64dc6b9780c7eea4c46ee8e87d63835c1b13680d
Glenn Reilly <glenn.reilly@gmail.com>
2019-02-18 22:59:27 +1100

    init commit

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,18 +1,43 @@
 import javafx.application.Application
 import javafx.application.Application.launch
 import javafx.scene.Scene
+import javafx.scene.control.Button
 import javafx.scene.control.Label
 import javafx.scene.layout.VBox
 import javafx.stage.Stage
+import tornadofx.action
+import tornadofx.show
 
 fun main(args: Array<String>) {
     launch(HelloWorld::class.java)
 }
 
-class HelloWorld : Application(){
+class HelloWorld : Application() {
     override fun start(primaryStage: Stage?) {
-        val layout = VBox().apply{
-            children.add(Label("Hello World"))
+
+
+        val layout = VBox().apply {
+
+            val introLabel = Label("Midi Test")
+
+            val resultLabel = Label("")
+
+            val playButton = Button("Play").apply {
+                action {
+                    resultLabel.setText("Playing..")
+                }
+            }
+
+            val stopButton = Button("Stop").apply {
+                action {
+                    resultLabel.setText("..Now stopped.")
+                }
+            }
+
+            children.add(introLabel)
+            children.add(playButton)
+            children.add(stopButton)
+            children.add(resultLabel)
         }
 
         primaryStage?.run {
